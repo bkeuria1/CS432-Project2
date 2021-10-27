f4bee574ca114801eb43bd272aeaa1d5ddc42dc3
XChronosCS <33588396+XChronosCS@users.noreply.github.com>
2021-10-26 22:14:11 -0400

    Adding code for Procedure 3
    Not sure if this is right just yet, but figured I would upload what I have.

---
+++ procedure_3.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE insert_student(sid in students.sid%type,
+ firstname in students.firstname%type, lastname in students.lastname%type, 
+ status in students.status%type, gpa in students.gpa%type, email in students.email%type)  
+AS
+BEGIN 
+	INSERT INTO students VALUES(sid, firstname, lastname, status, gpa, email);
+END
