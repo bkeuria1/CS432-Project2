1b55cc29b29c5f63656f123d01aff21a0b0d4465
eezor1 <eezor1@harveyv.binghamton.edu>
2021-10-27 14:59:34 -0400

    Pushing File For Review

--- project2/procedure_3.sql
+++ project2/procedure_3.sql
@@ -1,2 +1,8 @@
 set sqlblanklines on;
-CREATE PROCEDURE insert_student(sid in students.sid%type, firstname in students.firstname%type, lastname in students.lastname%type, status in students.status%type, gpa in students.gpa%type, email in students.email%type) AS BEGIN INSERT INTO students VALUES(sid, firstname, lastname, status, gpa, email); END
+set serveroutput on;
+create or replace package body srs as 
+PROCEDURE insert_student(sid in students.sid%type, firstname in students.firstname%type, lastname in students.lastname%type, status in students.status%type, gpa in students.gpa%type, email in students.email%type) IS
+BEGIN
+INSERT INTO students VALUES(sid, firstname, lastname, status, gpa, email);
+END;
+END;
