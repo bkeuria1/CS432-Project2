04edf5fb59c5de0f33b8bd8dd3db0bcdbd29b6d9
eezor1 <eezor1@harveyv.binghamton.edu>
2021-10-27 14:00:33 -0400

    Editing File

---
+++ project2/procedure_3.sql
@@ -0,0 +1,2 @@
+﻿set sqlblanklines on;
+CREATE PROCEDURE insert_student(sid in students.sid%type, firstname in students.firstname%type, lastname in students.lastname%type, status in students.status%type, gpa in students.gpa%type, email in students.email%type) AS BEGIN INSERT INTO students VALUES(sid, firstname, lastname, status, gpa, email); END
