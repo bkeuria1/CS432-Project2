0515ebe2ac81c042fca58ad779bb26d9caa1deb7
bkeuria1 <bkeuria1@binghamton.edu>
2021-10-30 16:32:03 -0400

    Finished 4 and 6

--- project2/q2.sql
+++ project2/q2.sql
@@ -17,6 +17,8 @@ procedure show_pre(c1 in out sys_refcursor);
 PROCEDURE insert_student(sid in students.sid%type, firstname in students.firstname%type, lastname in students.lastname%type, status in students.status%type, gpa in students.gpa%type, email in students.email%type);
 
 PROCEDURE get_student_info(p_sid in students.sid%type, c1 out sys_refcursor);
+
+PROCEDURE get_class_info(c_id in classes.classid%type, c1 out sys_refcursor);
 end;
 /
 show errors;
@@ -68,8 +70,8 @@ PROCEDURE get_student_info(p_sid in students.sid%type, c1 out sys_refcursor) as
 	enrollment_count number;
 	BEGIN
 
-	select count(*) into sid_count from students where p_sid in (sid);
-	select count(*) into enrollment_count from enrollments where p_sid in (enrollments.sid);
+	select count(*) into sid_count from students where p_sid = sid;
+	select count(*) into enrollment_count from enrollments where p_sid = enrollments.sid;
 	IF sid_count<1 THEN
 		dbms_output.put_line('The SID is invalid');
 		
@@ -78,11 +80,33 @@ PROCEDURE get_student_info(p_sid in students.sid%type, c1 out sys_refcursor) as
 	IF enrollment_count<1 THEN
 		dbms_output.put_line('The student has not taken any course');
 	END IF;
-		open c1 for select * from classes c,  enrollments e where p_sid = e.sid and c.classid = e.classid;
+		open c1 for select s.sid, s.firstname, s.gpa, e.classid, concat(c.dept_code, c.course_no) as COURSE , c.semester from students s, enrollments e, classes c  where
+		p_sid = s.sid and e.classid = c.classid and s.sid = e.sid;
 
 
 
 END;
+
+PROCEDURE get_class_info(c_id in classes.classid%type, c1 out sys_refcursor) as
+	class_count number;
+	enrollment_count number;
+	
+
+	BEGIN
+	select count(*) into class_count from classes where c_id = classid;
+	select count(*) into enrollment_count from enrollments where c_id = classid;
+	IF class_count<1 THEN
+		dbms_output.put_line('The classid is invalid');
+	END IF;
+	
+	IF enrollment_count<1 THEN
+		dbms_output.put_line('No student is enrolled in the class');
+	END IF;
+
+	open c1 for select classid, title, semester, year, sid, firstname, lastname, email from students inner join enrollments using (sid) inner join classes using (classid) inner join courses using(dept_code, course_no) where
+	c_id = classid;
+	
+	END;
 
 
 END;
